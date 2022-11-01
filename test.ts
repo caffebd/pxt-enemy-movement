ede5369bd10acd6a1364fd111cb19a341338e120
caffebd <caffebd@gmail.com>
2022-11-02 00:51:46 +0600

    updated test

--- test.ts
+++ test.ts
@@ -2,10 +2,10 @@
 
 let mySprite = sprites.create(sprites.castle.heroWalkFront1, SpriteKind.Enemy)
 mySprite.setPosition(20, 55)
-mySprite.vx = 20
+mySprite.setVelocity (20, 0)
 sprites.setPatrolPathX(mySprite, 50)
 
 let mySprite2 = sprites.create(sprites.builtin.forestBat7, SpriteKind.Enemy)
 mySprite2.setPosition(111, 55)
-mySprite2.vy = 20
+mySprite2.setVelocity (0, 20)
 sprites.setPatrolPathY(mySprite2, 50)
