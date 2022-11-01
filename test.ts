f1cc8abc64c7e0848bda6799b553b3088f38f093
caffebd <caffebd@gmail.com>
2022-11-02 00:41:37 +0600

    added test

--- test.ts
+++ test.ts
@@ -1,1 +1,11 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+
+let mySprite = sprites.create(sprites.castle.heroWalkFront1, SpriteKind.Enemy)
+mySprite.setPosition(20, 55)
+mySprite.vx = 20
+sprites.setPatrolPathX(mySprite, 50)
+
+let mySprite2 = sprites.create(sprites.builtin.forestBat7, SpriteKind.Enemy)
+mySprite2.setPosition(111, 55)
+mySprite2.vy = 20
+sprites.setPatrolPathY(mySprite2, 50)
